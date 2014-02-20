c9720af73bf29cdf7a142e6b93652f3bad0ccbdd
Herve Darritchon <herve.darritchon@gmail.com>
2014-02-20 22:28:20 +0100

    first test pass. Create a Sheet class to pass the first (empty cell)

---
+++ src/main/java/com/hervedarritchon/kata/Sheet.java
@@ -0,0 +1,14 @@
+package com.hervedarritchon.kata;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Hervé Darritchon
+ * Date: 20/02/2014
+ * Time: 22:11
+ * To change this template use File | Settings | File Templates.
+ */
+public class Sheet {
+    public String get(String cellId) {
+        return "";  //To change body of created methods use File | Settings | File Templates.
+    }
+}
--- src/test/java/com/hervedarritchon/kata/SheetTest.java
+++ src/test/java/com/hervedarritchon/kata/SheetTest.java
@@ -1,3 +1,5 @@
+package com.hervedarritchon.kata;
+
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
